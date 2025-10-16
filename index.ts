e99c297e2ed0e960e923969cfd85dc56b3743396
IsratErin <131592678+IsratErin@users.noreply.github.com>
2025-10-16 21:25:21 +0200

    new product entry

--- index.ts
+++ index.ts
@@ -9,3 +9,16 @@ app.use(express.json());
 app.listen(3000, () => {
   console.log(`Server is running on port 300`);
 });
+
+app.post("/products", async (req, res) => {
+  try {
+    const newProduct = await prisma.product.create({ data: req.body });
+    res.json(newProduct);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(500).send("Unknown error");
+    }
+  }
+});
